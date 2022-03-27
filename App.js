120d77c419dcdaf95c7155a328dbebc3a7d0eb82
NilanL <54152496+NilanL@users.noreply.github.com>
2022-03-27 06:11:25 -0400

    first commit

--- App.js
+++ App.js
@@ -1,5 +1,44 @@
+
+import React from 'react'; 
+import { StyleSheet, Text, View } from 'react-native'; 
+import { createStackNavigator } from "react-navigation-stack"; 
+import { createAppContainer } from "react-navigation"; 
+import LoginMobile from './screens/LoginMobile';
+import RegisterMobile from './screens/RegisterMobile';
+//import CardScreen from './screens/CardScreen'; 
+
+export default class App extends React.Component { 
+ render() { 
+ return <AppContainer />; 
+ } 
+}
+
+const AppNavigator = createStackNavigator({ 
+ Login: { 
+  screen: LoginMobile, 
+      navigationOptions: { 
+        header: null // Will hide header for HomePage 
+        } 
+      },
+  Register: { 
+  screen: RegisterMobile, 
+      navigationOptions: { 
+        header: null // Will hide header for HomePage 
+        } 
+      }  
+  },{ 
+  initialRouteName: "Login" 
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+/*
+export { default as LoginMobile } from "../screens/LoginMobile.js";
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+
 
 export default function App() {
   return (
@@ -17,4 +56,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
